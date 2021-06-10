996f1ba5ca2f74c709ca6710c600670d85b2ccd3
choijeongnam <cjn4336@gmail.com>
2021-06-10 14:24:24 +0900

    update passwordEncoder

--- src/main/java/kosta/web/mvc/config/WebSecurityConfig.java
+++ src/main/java/kosta/web/mvc/config/WebSecurityConfig.java
@@ -1,9 +1,12 @@
 package kosta.web.mvc.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,4 +16,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().permitAll();
 	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/kosta/web/mvc/member/service/MemberServiceImpl.java
+++ src/main/java/kosta/web/mvc/member/service/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package kosta.web.mvc.member.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kosta.web.mvc.member.domain.Member;
@@ -15,8 +16,13 @@ public class MemberServiceImpl implements MemberService {
 	@Autowired
 	MemberRepository memberRepository;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@Override
 	public void insert(Member member) {
+		member.setPwd(passwordEncoder.encode(member.getPwd()));
+		
 		memberRepository.save(member);
 	}
 
