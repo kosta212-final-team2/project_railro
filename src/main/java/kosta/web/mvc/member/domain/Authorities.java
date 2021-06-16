f37de4139ff6a18c43fffd43818652c69e54039a
choijeongnam <cjn4336@gmail.com>
2021-06-17 00:08:45 +0900

    update repository

--- src/main/java/kosta/web/mvc/member/domain/Authorities.java
+++ src/main/java/kosta/web/mvc/member/domain/Authorities.java
@@ -15,7 +15,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+@Entity(name="authorities")
 @Getter
 @Setter
 @NoArgsConstructor
--- src/main/java/kosta/web/mvc/member/repository/MemberRepository.java
+++ src/main/java/kosta/web/mvc/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package kosta.web.mvc.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import kosta.web.mvc.member.domain.Member;
@@ -13,12 +14,12 @@ public interface MemberRepository extends JpaRepository<Member, String>{
 	@Query(value = "SELECT * FROM MEMBER m WHERE m.MEMBER_ID = ?1", nativeQuery = true)
 	Member findByMemberId(String memberId);
 	
-	/*
-	 * @Query(value = "UPDATE * FROM MEMBER m WHERE m.MEMBER_ID = ?1", nativeQuery =
-	 * true) Member updateMember(Member member);
-	 */	
-	/*
-	 * @Query(value = "DELETE FROM MEMBER m WHERE m.MEMBER_ID = ?1", nativeQuery =
-	 * false) void deleteByMemberId(String memberId);
-	 */
+	@Modifying
+	@Query(value = "UPDATE * FROM MEMBER m WHERE m.MEMBER_ID = ?1", nativeQuery =true) 
+	Member updateMember(Member member);
+
+	@Modifying
+	@Query("DELETE FROM member m WHERE m.memberId = ?1") 
+	void deleteByMemberId(String memberId);
+
 }
