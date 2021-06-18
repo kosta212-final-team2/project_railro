4f8e93b04cae2b58bb6f8077a41c477afd883866
choi <cec0958@gmail.com>
2021-06-18 16:48:01 +0900

    css 적용

--- src/main/java/kosta/web/mvc/controller/HomeController.java
+++ src/main/java/kosta/web/mvc/controller/HomeController.java
@@ -14,54 +14,39 @@ import kosta.web.mvc.map.service.DetailedPlanService;
 
 @Controller
 public class HomeController {
-	@Autowired
-	DetailedPlanService detailService;
-	
-	@RequestMapping("/test")
-	public String test() {
-		return "page/test";
-	}
-	@RequestMapping("/")
-	public String index() {
-		System.out.println("call testpage");
-		return "page/main"; // WEB-INF/views/testpage.jsp
-	}
-	
-	@RequestMapping("/index")
-	public String thymeleafTest() { 
-		return "page/index"; 
-	}
-	
-	@RequestMapping("/categorySearch")
-	public String main() {
-		
-		return "page/map";
-	}
-	
-	@RequestMapping("/detailSave")
-	public String detailSave(DetailedPlanList list) {
-		System.out.println(list.getDetailedPlans());
-		detailService.insertAll(list);
-//		detailService.findByStaionPlan(99);
-		return "page/map";
-	}
-	
-	@RequestMapping("/updateForm")
-	public ModelAndView updateForm(int stationPlanNum) {
-		
-		List<DetailedPlan> list= detailService.findByStaionPlan(stationPlanNum);
-		
-		ModelAndView mv=new ModelAndView("page/updateForm", "detailData", list);
-		return mv;
-	}
-	
-	@RequestMapping("/update")
-	public String update(DetailedPlanList list, int stationPlanNum) {
-		for(DetailedPlan dplan:list.getDetailedPlans()) {
-			dplan.setStationPlan(new StationPlan(stationPlanNum));
-		}
-		detailService.updateAll(list, stationPlanNum);
-		return "page/map";
-	}
+	/*
+	 * @Autowired DetailedPlanService detailService;
+	 * 
+	 * @RequestMapping("/test") public String test() { return "page/test"; }
+	 * 
+	 * @RequestMapping("/") public String index() {
+	 * System.out.println("call testpage"); return "page/main"; //
+	 * WEB-INF/views/testpage.jsp }
+	 * 
+	 * @RequestMapping("/index") public String thymeleafTest() { return
+	 * "page/index"; }
+	 * 
+	 * @RequestMapping("/categorySearch") public String main() {
+	 * 
+	 * return "page/map"; }
+	 * 
+	 * @RequestMapping("/detailSave") public String detailSave(DetailedPlanList
+	 * list) { System.out.println(list.getDetailedPlans());
+	 * detailService.insertAll(list); // detailService.findByStaionPlan(99); return
+	 * "page/map"; }
+	 * 
+	 * @RequestMapping("/updateForm") public ModelAndView updateForm(int
+	 * stationPlanNum) {
+	 * 
+	 * List<DetailedPlan> list= detailService.findByStaionPlan(stationPlanNum);
+	 * 
+	 * ModelAndView mv=new ModelAndView("page/updateForm", "detailData", list);
+	 * return mv; }
+	 * 
+	 * @RequestMapping("/update") public String update(DetailedPlanList list, int
+	 * stationPlanNum) { for(DetailedPlan dplan:list.getDetailedPlans()) {
+	 * dplan.setStationPlan(new StationPlan(stationPlanNum)); }
+	 * detailService.updateAll(list, stationPlanNum); return "page/map"; }
+	 */
 
 }
