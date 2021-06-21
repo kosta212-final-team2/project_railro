49b3140302e4dea198c6157f7bb9f38eb04fc61d
choi <cec0958@gmail.com>
2021-06-21 13:54:41 +0900

    commit

--- src/main/java/kosta/web/mvc/map/controller/PlanController.java
+++ src/main/java/kosta/web/mvc/map/controller/PlanController.java
@@ -1,5 +1,15 @@
 package kosta.web.mvc.map.controller;
 
-public class PlanController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+@RequestMapping("/map")
+public class PlanController {
+	@RequestMapping("/planData")
+	public ModelAndView planData(Integer planId) {
+		ModelAndView mv=new ModelAndView("page/map/planData", "planId", planId);
+		return mv;
+	}
 }
